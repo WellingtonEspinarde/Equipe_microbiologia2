63a9be4c304cc8136f22295675e30bfccac45d72
WellingtonEspinarde <wellingtonespinarde@gmail.com>
2025-04-23 21:51:58 -0300

    Create Professor.php

---
+++ Classes/Professor.php
@@ -0,0 +1,23 @@
+<?php
+class Professor extends Pessoa {
+    private $numeroIdentificador;
+
+}
+
+    public function __construct($nome, $id, $dataNascimento, $cpf, $email, $endereco, $numeroIdentificador){
+
+    parent:: __construct($nome, $id, $dataNascimento, $cpf, $email, $endereco);
+    $this->numeroIdentificador = $numeroIdentificador;
+
+    }
+
+    public function getNumeroIdentificador(){
+    return $this-> numeroIdentificador;
+    }
+
+    public function setNumeroIdentificador($numeroIdentificador){
+    $this-> numeroIdentificador = $numeroIdentificador;
+    }
+
+
+?>

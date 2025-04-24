ab6401be0a905815497c71652d8564f34dbc904f
JAO123-ABC <jsilva1999@cs.up.edu.br>
2025-04-23 21:36:45 -0300

    Update Cadastro.php

--- Cadastro.php
+++ Cadastro.php
@@ -8,7 +8,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+<body class= "">
     <div class="container">
         <div class="row justify-content-center mt-5">
             <div class="col-md-6">

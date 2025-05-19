5760d31ecacc8c14a4f54126eb7bef4f9d73a507
JAO123-ABC <jsilva1999@cs.up.edu.br>
2025-05-18 23:42:55 -0300

    asd
    asd

--- Conector/Conexao.php
+++ Conector/Conexao.php
@@ -1,7 +1,7 @@
 <?php
 
 $hostname = "localhost";
-$bancodedados = "bandodedados" //alterar para o banco de dados ;
+$bancodedados = "usuario"
 $usuario = "root";
 $senha = "";
 
@@ -9,7 +9,5 @@ $mysql = new mysqli($hostname , $usuario, $senha, $bancodedados);
 if ($mysqli ->connect_errno ){
     echo "Falha ao conencatar (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-
-
 
 ?>

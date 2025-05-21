99e887b58997998c30b13f18fdbff2c4832a8794
WellingtonEspinarde <wellingtonespinarde@gmail.com>
2025-05-21 17:40:23 -0300

    concertando espaços

--- Cadastro.php
+++ Cadastro.php
@@ -4,12 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Usuário</title>
-  
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-
-
 
 <body>
 
@@ -33,8 +29,6 @@
 
                     <div class="card-body">
                         <form action="#" method="POST">
-
-                            
                             <div class="mb-3">
                                 <label for="nome" class="form-label">Nome Completo</label>
                                 <input type="text" class="form-control" id="nome" name="nome" required>
--- Home.php
+++ Home.php
@@ -6,6 +6,7 @@
     <title>Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
 
 <div class="row">

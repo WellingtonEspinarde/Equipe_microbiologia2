5797e3527581b5371c2403ff5ea4d1560a2f9c05
JAO123-ABC <jsilva1999@cs.up.edu.br>
2025-05-25 23:22:06 -0300

    Teste

--- PastaPHP/Dao/AlunoDao.php
+++ PastaPHP/Dao/AlunoDao.php
@@ -1,5 +1,23 @@
 <?php
 
+class AlunoDao{
+    private $pdo;
+}
+
+public function __construct($pdo){
+    $this->pdo = $pdo;
+}
+
+
+public function inserir(Aluno $aluno){
+    
+    $sql = "INSERT INTO  aluno(matricula, curso, anoLetivo) VALUES (:matricula, :curso, anoLetivo)";
+    $stmt = $this->pdo->prepare($sql);
+    $stmt ->bindValue(':nome', $aluno -> getCurso());
+    $stmt ->bindValue('anoLetivo', $aluno ->getanoLetivo());
+    $stmt ->execute();
+
+}
 
 
 

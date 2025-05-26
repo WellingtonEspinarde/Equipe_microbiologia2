013ce71283f4f89c06960b13d5875c133fb76d5b
JAO123-ABC <jsilva1999@cs.up.edu.br>
2025-05-26 20:44:32 -0300

    Reparando as pastas
    1a1

--- PastaPHP/Dao/ConnectionFactory.php
+++ PastaPHP/Dao/ConnectionFactory.php
@@ -5,7 +5,7 @@ class ConnectionFactory { // padrão singleton
     public static function getConnection() {
         if (!isset(self::$connection)) {
             $host = "localhost";
-            $dbName = "micro";
+            $dbName = "microbio";
             $user = "root";
             $pass = "";
             $port = 3306;
